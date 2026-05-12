95565d085563f3e90586785e71c58a697094ca4d
Austin Jones <austin.jones@uster.com>
2026-05-12 14:25:34 -0400

    Implement a wasm wrapper and page.
    Implement a wasm wrapper/runner for paddle. Consume this in a single
    html page.

---
+++ wasm/src/lib.rs
@@ -0,0 +1,58 @@
+use std::{cell::RefCell, rc::Rc};
+
+use js_sys::Function;
+use paddle_core::{
+    cursor::Cursor,
+    eval::{Env, value::Value},
+    lexer,
+};
+use wasm_bindgen::prelude::*;
+
+/*
+ * Le plan
+ *
+ * - make a result type
+ *     + maybe box?
+ * - store them in a vec
+ * - expose the vec with memory.js
+ * - render them
+ * - clear on render
+ */
+
+#[wasm_bindgen]
+pub enum Tag {
+    Err,
+    Print,
+    Ok,
+    Source,
+}
+
+#[wasm_bindgen]
+pub struct Runner {
+    env: Rc<RefCell<Env>>,
+}
+
+#[wasm_bindgen]
+impl Runner {
+    pub fn new() -> Self {
+        let env = Env::with_stdlib().expect("shouldn't fail");
+
+        Self { env }
+    }
+
+    pub fn run_code(&self, input: &str, callback: Function) {
+        let lexed = lexer::lex(&input);
+
+        let cursor = Cursor::new(&lexed, self.env.clone());
+
+        for res in cursor {
+            let (tag, content) = match res {
+                Err(err) => (Tag::Err, format!("ERROR: {:?}", err)),
+                Ok(Value::NoPrint) => (Tag::Print, "{...}".to_string()),
+                Ok(val) => (Tag::Ok, format!("{}", val)),
+            };
+
+            let _ = callback.call2(&JsValue::NULL, &tag.into(), &content.into());
+        }
+    }
+}
