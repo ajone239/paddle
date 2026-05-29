6ea8e22cc0e5d7998587ccb9f64ee91d5874abf5
Austin Jones <austin.jones@uster.com>
2026-05-29 17:09:15 -0400

    fix tests

--- core/src/eval/tests/tco.rs
+++ core/src/eval/tests/tco.rs
@@ -5,7 +5,7 @@ fn self_recursion_deep() {
     assert_eq!(
         eval_str_env(&[
             "(def (count n) (if (= n 0) 0 (count (- n 1))))",
-            "(count 5000)"
+            "(count 2500)"
         ]),
         num(0.0)
     );
@@ -17,7 +17,7 @@ fn mutual_recursion_deep() {
         eval_str_env(&[
             "(def (even? n) (if (= n 0) #t (odd? (- n 1))))",
             "(def (odd? n) (if (= n 0) #f (even? (- n 1))))",
-            "(even? 5000)"
+            "(even? 2500)"
         ]),
         Value::Bool(true)
     );
