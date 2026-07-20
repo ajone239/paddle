59886eb84ea8cfe0ce69f4b7c8eeab6a9d3ca35b
Austin Jones <austin.jones@uster.com>
2026-07-20 16:59:57 -0400

    wip

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,3 +1,8 @@
+use paddle_core::{
+    cursor::Cursor,
+    eval::{Env, value::Value},
+    lexer,
+};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,4 +13,20 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn run_code(input: &str) {
+    let env = Env::with_stdlib().expect("shouldn't fail");
+    let lexed = lexer::lex(&input);
+
+    let cursor = Cursor::new(&lexed, env);
+
+    for res in cursor {
+        match res {
+            Err(err) => alert(&format!("ERROR: {:?}", err)),
+            Ok(Value::NoPrint) => {}
+            Ok(val) => alert(&format!("{}", val)),
+        }
+    }
 }
