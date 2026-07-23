349769ea7963d94c3d24f406fd7a6884c532cc36
Austin Jones <ajones53.aj@gmail.com>
2026-07-23 08:30:23 -0400

    wip: like bad wip

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 use paddle_core::{
     cursor::Cursor,
     eval::{Env, value::Value},
@@ -5,28 +7,71 @@ use paddle_core::{
 };
 use wasm_bindgen::prelude::*;
 
+/*
+ * Le plan
+ *
+ * - make a result type
+ *     + maybe box?
+ * - store them in a vec
+ * - expose the vec with memory.js
+ * - render them
+ * - clear on render
+ */
+
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+pub enum RunResult {
+    Good(String),
+    Bad(String),
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+impl RunResult {
+    fn len(&self) -> usize {
+        match self {
+            RunResult::Good(s) => s.len(),
+            RunResult::Bad(s) => s.len(),
+        }
+    }
 }
 
 #[wasm_bindgen]
-pub fn run_code(input: &str) {
-    let env = Env::with_stdlib().expect("shouldn't fail");
-    let lexed = lexer::lex(&input);
+pub struct Runner {
+    env: Rc<RefCell<Env>>,
+    results: Vec<RunResult>,
+}
 
-    let cursor = Cursor::new(&lexed, env);
+#[wasm_bindgen]
+impl Runner {
+    pub fn new() -> Self {
+        let env = Env::with_stdlib().expect("shouldn't fail");
 
-    for res in cursor {
-        match res {
-            Err(err) => alert(&format!("ERROR: {:?}", err)),
-            Ok(Value::NoPrint) => {}
-            Ok(val) => alert(&format!("{}", val)),
+        Self {
+            env,
+            results: vec![],
+        }
+    }
+
+    pub fn results_len(&self) -> usize {
+        self.results.len()
+    }
+
+    pub fn results_ptr(&self) -> *const RunResult {
+        self.results.as_ptr()
+    }
+
+    pub fn run_code(&mut self, input: &str) {
+        let lexed = lexer::lex(&input);
+
+        let cursor = Cursor::new(&lexed, self.env.clone());
+
+        for res in cursor {
+            let rr = match res {
+                Err(err) => RunResult::Bad(format!("ERROR: {:?}", err)),
+                Ok(Value::NoPrint) => RunResult::Good("{...}".to_string()),
+                Ok(val) => RunResult::Good(format!("{}", val)),
+            };
+
+            self.results.push(rr);
         }
     }
 }
